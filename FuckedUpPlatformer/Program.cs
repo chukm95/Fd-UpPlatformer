c971e84cdee8e3850dfca50c6a8ed808e2119614
Chukm95 <jesse.m@live.nl>
2024-01-17 20:09:43 +0100

    initial commit

---
+++ FuckedUpPlatformer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FuckedUpPlatformer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
